f4529150a2fbd7f9580fc8427041afb2eca456dc
Zack Stout <zackstout@gmail.com>
2018-03-17 22:14:28 -0500

    got a triple level spirograph going

--- client.js
+++ client.js
@@ -3,6 +3,12 @@ var angle;
 var center1 = {x: 0, y: 0};
 var angle1;
 allPoints = [];
+var radius1;
+var radius2;
+
+var center2 = {x: 0, y:0};
+var radius3;
+var angle2;
 
 
 function setup() {
@@ -10,12 +16,15 @@ function setup() {
   background(101);
   angle = 0;
   angle1 = 0;
-
+  angle2 = 0;
+  radius1 = 150;
+  radius2 = 40;
+  radius3 = 20;
 
   push();
   translate(width/2, height/2);
   rotate(angle);
-  ellipse(150, 0, 5, 5);
+  ellipse(radius1, 0, 5, 5);
   pop();
 }
 
@@ -25,17 +34,9 @@ function draw() {
   push();
   translate(width/2, height/2);
   rotate(angle);
-
-  center1.x = 150 * Math.cos(angle);
-  center1.y = 150 * Math.sin(angle);
-  ellipse(150, 0, 5, 5);
-
-  // push();
-  // translate(center1.x, center1.y);
-  // rotate(angle1);
-  // ellipse(40, 0, 4, 4);
-  //
-  // pop();
+  center1.x = radius1 * Math.cos(angle);
+  center1.y = radius1 * Math.sin(angle);
+  ellipse(radius1, 0, 5, 5);
   pop();
 
   push();
@@ -44,17 +45,39 @@ function draw() {
   // Ok if we do this, we draw the original planet's path:
   // allPoints.push({x: x1, y: y1});
 
-  allPoints.push({x: x1 + 40 * Math.cos(angle1), y: y1 + 40 * Math.sin(angle1)});
-
+  center2.x = x1 + radius2 * Math.cos(angle1);
+  center2.y = y1 + radius2 * Math.sin(angle1);
+  // allPoints.push({x: center2.x, y: center2.y});
   translate(x1, y1);
   rotate(angle1);
-  ellipse(40, 0, 4, 4);
+  ellipse(radius2, 0, 4, 4);
   pop();
+
+
+  // well it's working.....I have no idea why this is the proper translation though:
+  push();
+  var x2 =  center2.x;
+  var y2 =  center2.y;
+
+  allPoints.push({x: x2 + radius3 * Math.cos(angle2), y: y2 + radius3 * Math.sin(angle2)});
+
+  translate(x2, y2);
+  rotate(angle2);
+  ellipse(radius3, 0, 4, 4);
+  pop();
+
+  // console.log(x2, y2);
+
+
+
 
   // console.log(center1);
 
+
+  // if angle1 is n*angle, then will createa  flower with (n - 1) petals!!!
   angle += 0.01;
-  angle1 += 0.05;
+  angle1 += 0.077;
+  angle2 += 0.1;
 
 
   allPoints.forEach(function(point) {
